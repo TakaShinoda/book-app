f6d346d6caf03d0ada774f37b67f5bf508b49474
Takahiro <tkhr_621@docomo.ne.jp>
2024-12-29 03:31:20 +0900

    ユニットテストの実装

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,23 @@ pub async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
+#[tokio::test]
+async fn hello_world_works() {
+    let response_message = hello_world().await;
+
+    assert_eq!(response_message, "Hello, world");
+}
+
+// 非同期関数のテスト実行に tokio::test マクロを使う
+#[tokio::test]
+async fn health_check_works() {
+    // health_check 関数を呼び出す。await して結果を得る
+    let status_code = health_check().await;
+
+    // 関数を実行した結果が、200 OK であることを確かめる
+    assert_eq!(status_code, StatusCode::OK);
+}
+
 // tokio ランタイム上で動かすために必要なマクロ、このマクロを使用することで main 関数を非同期化
 #[tokio::main]
 async fn main() -> Result<()> {
