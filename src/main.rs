189fd0452fc0353ee4f1d68cd771bb6958044801
Takahiro <tkhr_621@docomo.ne.jp>
2024-12-29 03:09:03 +0900

    ヘルスチェックの実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
 // ハンドラ
@@ -8,21 +9,26 @@ async fn hello_world() -> &'static str {
     "Hello, world"
 }
 
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
 // tokio ランタイム上で動かすために必要なマクロ、このマクロを使用することで main 関数を非同期化
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // ルーターの設定
-    // GET: /hello に対して、hello_world 関数を実行する
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new()
+        .route("/hello", get(hello_world))
+        .route("/health", get(health_check));
 
     // ローカルホスト 8080 番ポートでリクエストを待ち受ける
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
 
     // 上記で設定したアドレスでバインドしたリスナーを立ち上げる
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
 
     println!("Listening on {}", addr);
 
     // 起動する際に、ルーターを axum サーバーに登録する
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
