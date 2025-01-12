3814205a73cebbc5efcd4024182cc68d6a0032c5
Takahiro <tkhr_621@docomo.ne.jp>
2025-01-13 02:33:23 +0900

    adapter のテストを作成

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -73,3 +73,54 @@ impl BookRepository for BookRepositoryImpl {
         Ok(row.map(Book::from))
     }
 }
+
+// #[cfg(test)] アトリビュートを tests モジュールに付与すると
+// cargo test した時のみ実装させるテストを記述できる
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[sqlx::test]
+    async fn test_register_book(pool: sqlx::PgPool) -> anyhow::Result<()> {
+        // BookRepositoryImpl を初期化
+        let repo = BookRepositoryImpl::new(ConnectionPool::new(pool));
+
+        // 投入するためのデータを作成
+        let book = CreateBook {
+            title: "Test Title".into(),
+            author: "Test Author".into(),
+            isbn: "Test ISBN".into(),
+            description: "Test Description".into(),
+        };
+
+        // 蔵書データ投入すると正常終了することを確認
+        repo.create(book).await?;
+
+        // 蔵書の一覧を取得すると1件だけ取得できることを確認
+        let res = repo.find_all().await?;
+        assert_eq!(res.len(), 1);
+
+        // 蔵書の一覧の最初のデータから蔵書IDを取得し、
+        // find_by_id メソッドでその蔵書データを取得できることを確認
+        let book_id = res[0].id;
+        let res = repo.find_by_id(book_id).await?;
+        assert!(res.is_some());
+
+        // 取得した蔵書データが CreateBook で投入した
+        // 蔵書データと一致することを確認
+        let Book {
+            id,
+            title,
+            author,
+            isbn,
+            description,
+        } = res.unwrap();
+        assert_eq!(id, book_id);
+        assert_eq!(title, "Test Title");
+        assert_eq!(author, "Test Author");
+        assert_eq!(isbn, "Test ISBN");
+        assert_eq!(description, "Test Description");
+
+        Ok(())
+    }
+}
